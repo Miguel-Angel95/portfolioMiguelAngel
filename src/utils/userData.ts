9a5bb6e0fd26e87c0ad85bd839adbf596f59b05a
Miguel <miguelangeloliveira95@gmail.com>
2023-07-20 20:57:51 -0300

    add: componesnts, pages, utils, style

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Miguel Angel",
+  githubUser: "Miguel-Angel95",
+  linkedinUser: "miguel-angel-oliveira",
+  whatsappNumber: "81 99707-0869",
+  emailUser: "miguelangeloliveira95@gmail.com",
+  instagramUser: "miguelangeloliveira95",
+  facebookUser: "profile.php?id=100002631627394",
+};
